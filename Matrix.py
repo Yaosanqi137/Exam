6bb59ab0c0b43fc6f2c5b00bcfbdc966e5d37118
Yaosanqi137 <zxc18023571263@outlook.com>
2024-09-28 15:18:06 +0800

    第四题完成

---
+++ Matrix.py
@@ -0,0 +1,24 @@
+def lcm(num1, num2):
+    check = max(num1, num2)
+    while True:
+        if check % num1 == 0 and check % num2 == 0 :
+            break
+        else:
+            check += 1
+    return check
+
+def matrix(n):
+    columnList = []
+    for column in range(1,n + 1):
+        lineList = []
+        for line in range(1,n + 1):
+            lineList.append(lcm(line, column))
+        columnList.append(lineList)
+    for i in range(0, len(columnList)):
+        for j in range(0, len(columnList[i])):
+            print(columnList[i][j], end="   ")
+        print("\n")
+
+if __name__ == '__main__':
+    n = int(input())
+    matrix(n)
