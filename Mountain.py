33af046ec6b0a65529411219ec0aba3af19a11a3
Yaosanqi137 <zxc18023571263@outlook.com>
2024-09-28 11:17:53 +0800

    第三题完成

---
+++ Mountain.py
@@ -0,0 +1,10 @@
+def climbed(altitudes):
+    climbed = 0
+    for m in range(1, len(altitudes) - 1):
+        if altitudes[m - 1] < altitudes[m] and altitudes[m + 1] < altitudes[m] :
+            climbed += 1
+    print(climbed)
+
+if __name__ == '__main__':
+    altInput = list(map(float, input().split()))
+    climbed(altInput)
