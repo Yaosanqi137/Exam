7aab69780d95cebb7a9d1c7989a4875cac69baa5
Yaosanqi137 <zxc18023571263@outlook.com>
2024-10-01 11:50:58 +0800

    修改爬山问题的函数名

--- 3.Mountain.py
+++ 3.Mountain.py
@@ -1,4 +1,4 @@
-def climbed(altitudes):
+def mountCount(altitudes):
     climbed = 0
     for m in range(1, len(altitudes) - 1):
         if altitudes[m - 1] < altitudes[m] and altitudes[m + 1] < altitudes[m] :
@@ -7,4 +7,4 @@ def climbed(altitudes):
 
 if __name__ == '__main__':
     altInput = list(map(float, input().split()))
-    climbed(altInput)
+    mountCount(altInput)
