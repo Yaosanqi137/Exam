f4af1455e4c9e709ba1b8ddea4f88f43dc65edff
Yaosanqi137 <99163721+Yaosanqi137@users.noreply.github.com>
2024-10-13 17:42:43 +0800

    少写一个空格

--- 2.StorePrice.py
+++ 2.StorePrice.py
@@ -1,5 +1,5 @@
 def price(amount, priceList):
-    backup = priceList[:] #创建副本列表
+    backup = priceList[:] # 创建副本列表
     for n in range(0, amount):
         if n - 1 == -1:
             priceList[n] = (backup[n] + backup[n + 1]) // 2 # 对第一个店铺价格的处理
